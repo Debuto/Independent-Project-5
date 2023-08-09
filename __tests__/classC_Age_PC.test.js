70d07c87455fa579fe21c8002cdddae8b9789a0a
Debo <dwdeante0726@gmail.com>
2023-08-08 21:19:14 -0700

    This will remove UI.

--- __tests__/classC_Age_PC.test.js
+++ __tests__/classC_Age_PC.test.js
@@ -1,7 +1,15 @@
 const { Planet, AgeCalculator } = require('../src/js/classC_Age_PC');
 
-describe('Planet and AgeCalculator', () => {
-  describe('Planet class', () => {
+describe('Planet', () => {
+  describe('constructor()', () => {
+    it('should create a Planet object with name and conversionFactor properties', () => {
+      const myPlanetObject = new Planet("Mars", 1.88)
+      expect(myPlanetObject.name).toEqual("Mars")
+      expect(myPlanetObject.conversionFactor).toEqual(1.88)
+    })
+  })
+
+  describe('calculateAge()', () => {
     it('should correctly calculate age for Mercury', () => {
       const mercury = new Planet('Mercury', 0.24);
       expect(mercury.calculateAge(100)).toBe('416.67');
@@ -22,8 +30,18 @@ describe('Planet and AgeCalculator', () => {
       expect(jupiter.calculateAge(100)).toBe('8.43');
     });
   });
+});
 
-  describe('AgeCalculator class', () => {
+describe('AgeCalculator', () => {
+  describe('constructor()', () => {
+    it('should create AgeCalculator objects with a "planets" property.', () => {
+      const myAgeCalculator = new AgeCalculator();
+      expect(myAgeCalculator.planets[0].name).toEqual("Mercury")
+      expect(myAgeCalculator.planets[0].conversionFactor).toEqual(.24)
+    })
+  })
+
+  describe('calculatePlanetAges()', () => {
     it('should correctly calculate ages for all planets', () => {
       const ageCalculator = new AgeCalculator();
       const planetAges = ageCalculator.calculatePlanetAges(100);
@@ -34,19 +52,19 @@ describe('Planet and AgeCalculator', () => {
         Jupiter: '8.43',
       });
     });
-  });
 
-  it('should display error message for invalid input', () => {
-      const ageCalculator = new AgeCalculator();
-
-      const promptMock = jest.spyOn(window, 'prompt');
-      promptMock.mockReturnValueOnce('invalid input');
-
-      ageCalculator.main();
-
-      const outputDiv = window.document.getElementById('output');
-      const displayedText = outputDiv.textContent;
-
-      expect(displayedText).toContain('Invalid input. Please enter a valid number.');
-    });
+    describe('pastYears()', () => {
+      it("should return the difference in years between a past age and the present age for all planets", () => {
+        const ageCalculator = new AgeCalculator();
+        const planetAges = ageCalculator.pastYears(100, 90);
+        expect(planetAges).toEqual({
+          Mercury: '41.66',
+          Venus: '16.13',
+          Mars: '5.32',
+          Jupiter: '.84',
+        });
+      })
+    })
+  })
+ 
 });
--- src/js/classC_Age_PC.js
+++ src/js/classC_Age_PC.js
@@ -18,7 +18,7 @@ class AgeCalculator {
       new Planet("Jupiter", 11.86)
     ];
   }
-
+  
   calculatePlanetAges(earthAge) {
     const planetAges = {};
     this.planets.forEach(planet => {
@@ -27,32 +27,14 @@ class AgeCalculator {
     return planetAges;
   }
 
-  displayOutput(outputText) {
-    const outputDiv = document.getElementById('output');
-    outputDiv.textContent = outputText;
-  }
-
-  main() {
-    const earthAge = parseFloat(prompt('Enter your age in Earth years:'));
-
-    if (isNaN(earthAge)) {
-      this.displayOutput('Invalid input. Please enter a valid number.');
-      return;
-    }
-
-    const planetAges = this.calculatePlanetAges(earthAge);
-
-    let outputText = 'Your age on different planets:\n';
-    for (const planet in planetAges) {
-      outputText += `${planet}: ${planetAges[planet]} years\n`;
-    }
-
-    this.displayOutput(outputText);
+  pastYears(presentAge, pastAge){
+    const planetAges = {};
+    this.planets.forEach(planet => {
+      planetAges[planet.name] = planet.calculateAge(earthAge);
+    });
+    return planetAges;
   }
 }
-
-const ageCalculator = new AgeCalculator();
-ageCalculator.main();
 
 module.exports = {
   Planet,
